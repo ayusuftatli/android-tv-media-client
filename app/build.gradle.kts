3d7cfa8ca121fb58d1a44ea26f608ebe4fd7f4a2
Yusuf Tatli <a.yusuftatli@gmail.com>
2026-02-25 14:23:38 +0100

    fixe the crash issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
 
     // Media3 ExoPlayer
     implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-exoplayer-hls:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
     implementation("androidx.media3:media3-common:1.4.1")
 
